043e807252830844a6335a110831695fe7d64326
alexiahassan <alexiahassan@tamu.edu>
2024-05-22 23:57:33 -0500

    added icons

---
+++ dist/script.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function hideLandingText() {
+  var x = document.getElementById("landingText");
+  console.log('hi hello');
+  x.style.display = "none";
+}
+
+function showLandingText() {
+  var x = document.getElementById("landingText");
+  console.log('hi hello');
+  x.style.display = "block";
+}
+
+function transitionToAbout() {}
